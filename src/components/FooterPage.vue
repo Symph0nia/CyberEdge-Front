c0e2ebf9844fd3e7d63799ac88dc9827d9c757b4
ZacharyZcR <2903735704@qq.com>
2024-12-10 20:24:53 +0800

    perf: 样式优化

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,17 +1,15 @@
 <template>
   <footer class="bg-gray-800 text-center py-8">
     <div class="font-sans">
-      <p class="tracking-wide text-gray-200 text-sm font-light mb-2">
-        &copy; 2024 CyberEdge 🌐 综合扫描器. 保留所有权利.
+      <p class="tracking-wide text-gray-200 text-sm font-light mb-2 flex items-center justify-center">
+        &copy; 2024 CyberEdge
+        <i class="ri-global-line ml-1"></i>
+        综合扫描器. 保留所有权利.
       </p>
 
-      <p class="tracking-wide text-gray-400 text-xs font-light leading-relaxed">
+      <p class="tracking-wide text-gray-400 text-xs font-light leading-relaxed flex items-center justify-center">
         致力于保护您的网络安全与隐私
-        <span
-          class="inline-block ml-1 transform hover:scale-105 transition-transform duration-200"
-        >
-          🔒
-        </span>
+        <i class="ri-shield-keyhole-line ml-1 transform hover:scale-105 transition-transform duration-200"></i>
       </p>
     </div>
   </footer>
